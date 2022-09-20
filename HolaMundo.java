c11beba880886b77c88d9e70508b28daaac41be6
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-09-20 22:55:44 +0200

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,12 @@
+package holamundo;
+/**
+ * Este mi primer Hola Mundo en 1º de DAM de 2022/2023 en I.E.S. Belén
+ * @author AdrianoDíazBenítez
+ */
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola, Mundo");
+        System.out.println("Java version: " + 
+                System.getProperty("java.version"));
+    }  
+}
