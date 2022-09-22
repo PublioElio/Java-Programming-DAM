4fbc5e239f8dc2eec58d6f1c09f3401e9dc9cf94
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-09-22 15:47:47 +0200

    Add files via upload

---
+++ RoundNumber.java
@@ -0,0 +1,27 @@
+package roundnumber;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * This program asks the user for a decimal number and rounds it
+ * @author Adriano Díaz Benítez <Adriano.Díaz>
+ */
+public class RoundNumber {
+    public static void main(String[] args)
+    {
+        double num;
+        long rounded;
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US); // to use period instead of comma for decimals
+        System.out.println("Enter a decimal number "
+                + "(use period, not comma): ");
+        num = sc.nextDouble();
+        rounded = Math.round(num);
+        System.out.println("Decimal number rounded: " + rounded);
+        num += 0.5;
+        System.out.println("\nAlternative solution:\n"
+                + "----------------------------------------------------------\n"
+                + "Decimal number rounded: " + (int)num);
+    }
+}
