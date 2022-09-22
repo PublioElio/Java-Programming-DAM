d01ebd0c442ff0d3c909fc5be5733beb879b1b44
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-09-22 23:52:14 +0200

    Add files via upload

---
+++ SecondGradePolynomial.java
@@ -0,0 +1,24 @@
+package secondgradepolynomial;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Adriano Díaz <Adriano.Díaz>
+ */
+public class SecondGradePolynomial {
+    public static void main(String[] args){
+        int a, b, c, x, y;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value of 'a':");
+        a = sc.nextInt();
+        System.out.println("Enter value of 'b':");
+        b = sc.nextInt();
+        System.out.println("Enter value of 'c':");
+        c = sc.nextInt();
+        System.out.println("Enter value of 'x':");
+        x = sc.nextInt();
+        y = ((a * (x * x)) + (b * x) + c);
+        System.out.println("The result of 'y = ax^2 + bx + c' is: " + y);
+    }
+}
