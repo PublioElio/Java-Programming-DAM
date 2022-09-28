61501f6ee3716ae2d3f21ad5caf8a81fafd687e0
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-09-28 13:00:55 +0200

    Add files via upload

--- primer_trimestre/unit1/src/appact_15/SecondGradePolynomial.java
+++ primer_trimestre/unit1/src/appact_15/SecondGradePolynomial.java
@@ -3,12 +3,14 @@ package appact_15;
 import java.util.Scanner;
 
 /**
- *
+ * This program calculates a 2º grade polynomial, first, knowing 'x', later
+ * calculating value of 'x'
  * @author Adriano Díaz <Adriano.Díaz>
  */
 public class SecondGradePolynomial {
     public static void main(String[] args){
-        int a, b, c, x, y;
+        int a, b, c;
+        double d, x, y;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter value of 'a':");
         a = sc.nextInt();
@@ -20,5 +22,18 @@ public class SecondGradePolynomial {
         x = sc.nextInt();
         y = ((a * (x * x)) + (b * x) + c);
         System.out.println("The result of 'y = ax^2 + bx + c' is: " + y);
+        System.out.println("""
+                           -----------------------------------------------------
+                           Without 'x':""");
+        d = (b * b) - (4 * a * c);
+        x = (-b - Math.sqrt(d)) / (2 * a);
+        System.out.printf("\nFirst result of 'x' %,.2f\n", x);
+        y = ((a * (x * x)) + (b * x) + c);
+        System.out.printf("\nValue of 'y': %,.2f\n", y);
+        x = (-b + Math.sqrt(d)) / (2 * a);
+        System.out.printf("\nSecond result of 'x' %,.2f\n", x);
+        y = ((a * (x * x)) + (b * x) + c);
+        System.out.printf("\nValue of 'y': %,.2f\n", y);
+        
     }
 }
