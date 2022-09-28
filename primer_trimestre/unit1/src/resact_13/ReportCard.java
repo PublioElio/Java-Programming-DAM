df314517d7985913cbefdda8f3d935f68ae21ee8
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-09-28 13:33:36 +0200

    Add files via upload

--- primer_trimestre/unit1/src/resact_13/ReportCard.java
+++ primer_trimestre/unit1/src/resact_13/ReportCard.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 
 /**
  * This progam calculates the report card and the academic record of a student
+ *
  * @author Adriano Díaz Benítez <Adriano.Díaz>
  */
 public class ReportCard {
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         int rating1, rating2, rating3, reportCard;
         double academicRecord;
         Scanner sc = new Scanner(System.in);
@@ -19,8 +20,8 @@ public class ReportCard {
         System.out.println("Enter califications for the third assessment: ");
         rating3 = sc.nextInt();
         academicRecord = (rating1 + rating2 + rating3) / 3.0;
-        reportCard = (int)academicRecord;
-        System.out.println("-----------------------------------\n Report Card: " 
-                + reportCard + "\t\tAcademic Record: " + academicRecord);         
+        reportCard = (int) academicRecord;
+        System.out.println("-----------------------------------\n Report Card: "
+                + reportCard + "\t\tAcademic Record: " + academicRecord);
     }
 }
