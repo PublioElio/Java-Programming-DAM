84f68e6a07447a789fd91ecf91d9ed20e00e7542
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-09-28 13:13:58 +0200

    Add files via upload

--- primer_trimestre/unit1/src/appact_17/DistanceCalculator.java
+++ primer_trimestre/unit1/src/appact_17/DistanceCalculator.java
@@ -3,25 +3,27 @@ package appact_17;
 import java.util.Scanner;
 
 /**
- * This program calculates distance in total centimeters from an imput of 
+ * This program calculates distance in total centimeters from an imput of
  * milimeters, centimeters and meters
+ *
  * @author Adriano Díaz <Adriano.Díaz>
  */
 public class DistanceCalculator {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         int m;
-        double cm, ml;
+        double cm, mm;
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.println("Enter milimeters: ");
-        ml = sc.nextDouble();
+        mm = sc.nextDouble();
         System.out.println("Enter centimeters: ");
         cm = sc.nextDouble();
         System.out.println("Enter meters: ");
         m = sc.nextInt();
-        cm += (m * 100) + (ml / 10);
+        cm += (m * 100) + (mm / 10);
         System.out.printf("""
                           Te total distance in centimeters is: %,.2f m.
                           """, cm);
-    }        
+    }
 }
