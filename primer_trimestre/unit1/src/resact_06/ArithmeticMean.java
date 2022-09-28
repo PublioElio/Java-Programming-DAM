d0f659f6a7131c5936027c1913c0984ecb394a24
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-09-28 13:27:13 +0200

    Add files via upload

--- primer_trimestre/unit1/src/resact_06/ArithmeticMean.java
+++ primer_trimestre/unit1/src/resact_06/ArithmeticMean.java
@@ -3,17 +3,18 @@ package resact_06;
 import java.util.Scanner;
 
 /**
- * This program calculates the arithmetic mean 
- * of two ratings entered by the user.
+ * This program calculates the arithmetic mean of two ratings entered by the
+ * user.
+ *
  * @author Adriano Díaz Benítez <Adriano.Díaz>
  */
 public class ArithmeticMean {
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         int rating1, rating2;
         double mean;
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.println("Enter first score: ");
         rating1 = sc.nextInt();
         System.out.println("Enter second score: ");
