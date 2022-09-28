17530d04f5b8e79cb57daf993b2112c9f326a213
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-09-28 13:52:42 +0200

    Add files via upload

--- primer_trimestre/unit1/src/appact_16/TimeCalculator.java
+++ primer_trimestre/unit1/src/appact_16/TimeCalculator.java
@@ -16,11 +16,9 @@ public class TimeCalculator {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter any number of seconds: ");
         sec = sc.nextInt();
-        
         hours = sec / 3600;
         min = (sec % 3600) / 60;
         sec %= 60;
-
         System.out.printf("Total: %,.2f h. %,.2f min. %d sec.\n",
                 hours, min, sec);
     }
