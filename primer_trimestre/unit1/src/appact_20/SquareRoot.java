c95d10eb1fc12d9049f47910d3d630853f3e0a90
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-09-28 14:26:42 +0200

    Add files via upload

--- primer_trimestre/unit1/src/appact_20/SquareRoot.java
+++ primer_trimestre/unit1/src/appact_20/SquareRoot.java
@@ -1,9 +1,7 @@
 package appact_20;
 
-import java.util.Scanner;
-
 /**
- * This program calculates the square root of a given number
+ * This program calculates the square root of a given number, but without import
  *
  * @author Adriano Díaz <Adriano.Díaz>
  */
@@ -11,10 +9,10 @@ public class SquareRoot {
 
     public static void main(String[] args) {
         double num;
-        Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number to calculate square root: ");
+        java.util.Scanner sc = new java.util.Scanner(System.in);
         num = sc.nextDouble();
         System.out.println("The square root of " + num
-                + " es " + Math.sqrt(num));
+                + " is " + java.lang.Math.sqrt(num));
     }
 }
