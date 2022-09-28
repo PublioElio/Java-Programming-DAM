443caa52112e3d68ce6fd2723a3b5c67ac5807de
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-09-28 13:11:46 +0200

    Add files via upload

--- primer_trimestre/unit1/src/appact_15/SecondGradePolynomial.java
+++ primer_trimestre/unit1/src/appact_15/SecondGradePolynomial.java
@@ -3,12 +3,14 @@ package appact_15;
 import java.util.Scanner;
 
 /**
- * This program solves a 2º grade polynomial: first, with the value of 'x', 
+ * This program solves a 2º grade polynomial: first, with the value of 'x',
  * later calculating value of 'x'
+ *
  * @author Adriano Díaz <Adriano.Díaz>
  */
 public class SecondGradePolynomial {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         int a, b, c;
         double d, x, y;
         Scanner sc = new Scanner(System.in);
@@ -34,6 +36,6 @@ public class SecondGradePolynomial {
         System.out.printf("\nSecond result of 'x' %,.2f\n", x);
         y = ((a * (x * x)) + (b * x) + c);
         System.out.printf("\nValue of 'y': %,.2f\n", y);
-        
+
     }
 }
