c39cfd1693918d773b054b187f9f86031773b641
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-09-28 13:16:43 +0200

    Add files via upload

--- primer_trimestre/unit1/src/appact_20/SquareRoot.java
+++ primer_trimestre/unit1/src/appact_20/SquareRoot.java
@@ -4,15 +4,17 @@ import java.util.Scanner;
 
 /**
  * This program calculates the square root of a given number
+ *
  * @author Adriano Díaz <Adriano.Díaz>
  */
 public class SquareRoot {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         double num;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number to calculate square root: ");
         num = sc.nextDouble();
-        System.out.println("The square root of " + num 
+        System.out.println("The square root of " + num
                 + " es " + Math.sqrt(num));
     }
 }
