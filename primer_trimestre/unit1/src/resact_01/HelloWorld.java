b83e06a04dbc7e252e19fa1c81d42f541306b80b
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-09-28 13:23:42 +0200

    Add files via upload

--- primer_trimestre/unit1/src/resact_01/HelloWorld.java
+++ primer_trimestre/unit1/src/resact_01/HelloWorld.java
@@ -1,15 +1,18 @@
 package resact_01;
+
 /**
  * This is my first Hello World (also shows the current JDK installed and the
  * local time on the system)
+ *
  * @author AdrianoDíazBenítez
  */
 public class HelloWorld {
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Java version: " + 
-                System.getProperty("java.version"));
+        System.out.println("Java version: "
+                + System.getProperty("java.version"));
         java.time.LocalTime whatTimeIs = java.time.LocalTime.now();
         System.out.println("System local time is: " + whatTimeIs);
-    }  
+    }
 }
