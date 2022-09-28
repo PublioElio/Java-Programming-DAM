790099d7cbeac1f14e67ef93cf78758cbd76c18c
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-09-28 14:13:03 +0200

    Add files via upload

--- primer_trimestre/unit1/src/appact_16/TimeCalculator.java
+++ primer_trimestre/unit1/src/appact_16/TimeCalculator.java
@@ -19,7 +19,7 @@ public class TimeCalculator {
         hours = sec / 3600;
         min = (sec % 3600) / 60;
         sec %= 60;
-        System.out.printf("Total: %,.2f h. %,.2f min. %d sec.\n",
-                hours, min, sec);
+        System.out.printf("Total: %d h. %d min. %d sec.\n",
+                (int)hours, (int)min, sec);
     }
 }
