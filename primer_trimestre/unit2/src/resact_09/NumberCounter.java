2a24d3d1a541c13a99e3d129ad39a8f1521139d1
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-09-28 22:14:32 +0200

    Add files via upload

---
+++ primer_trimestre/unit2/src/resact_09/NumberCounter.java
@@ -0,0 +1,31 @@
+package resact_09;
+
+import java.util.Scanner;
+
+/**
+ * This program tells how many numbers has an integer between 0 and 99.999
+ *
+ * @author Adriano Díaz <Adriano.Díaz>
+ */
+public class NumberCounter {
+
+    public static void main(String[] args) {
+        int num;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter an integer number between "
+                + "-99.999 and 99.999: ");
+        num = sc.nextInt();
+        num = num < 0 ? num * -1 : num;
+        if (num < 10) {
+            System.out.println("That number has 1 digit");
+        } else if (num < 100) {
+            System.out.println("That number has 2 digits");
+        } else if (num < 1000) {
+            System.out.println("That number has 3 digits");
+        } else if (num < 10000) {
+            System.out.println("That number has 4 digits");
+        } else if (num < 100000) {
+            System.out.println("That number has 5 digits");
+        }
+    }
+}
