3747d1d73e28c363faf18ebc5e277a56acd31d6b
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-09-28 21:44:38 +0200

    Add files via upload

---
+++ primer_trimestre/unit2/src/resact_01_02_03_06/CompareNumbers.java
@@ -0,0 +1,29 @@
+package resact_01_02_03_06;
+
+import java.util.Scanner;
+
+/**
+ * This program compares two numbers using the if-else statement
+ * @author Adriano Díaz <Adriano.Díaz>
+ */
+public class CompareNumbers {
+    public static void main(String[] args){
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a first integer value: ");
+        a = sc.nextInt();
+        System.out.println("Enter a second integer value: ");
+        b = sc.nextInt();
+        if ((a % 2) == 0)
+            System.out.printf("\n'%d' is an even number\n", a);
+        else
+            System.out.printf("\n'%d' isn't an even number\n", a);
+        if (a == b)
+            System.out.printf("\n'%d' and '%d' are the same number\n",
+                    a, b);
+        else if (a > b)
+            System.out.printf("\n'%d' is greater than '%d'\n",a,b);
+        else
+            System.out.printf("\n'%d' is greater than '%d'\n",b,a);
+    }        
+}
