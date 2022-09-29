211795c5027c86bc63aa3c70b4936d014e34eb19
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-09-29 17:46:51 +0200

    Add files via upload

--- primer_trimestre/unit2/src/resact_12/CheckDate.java
+++ primer_trimestre/unit2/src/resact_12/CheckDate.java
@@ -3,6 +3,7 @@ package resact_12;
 import java.util.Scanner;
 
 /**
+ * This program checks if the imput date is valid
  *
  * @author Adriano Díaz Benítez <Adriano.Díaz>
  */
@@ -12,20 +13,34 @@ public class CheckDate {
         int day, month, year;
         boolean date;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter day");
+        System.out.print("Enter day: ");
         day = sc.nextInt();
-        System.out.print("Enter month");
+        System.out.print("Enter month: ");
         month = sc.nextInt();
-        System.out.print("Enter year");
+        System.out.print("Enter year: ");
         year = sc.nextInt();
-        if (year == 0) {
-            System.out.print("'0' isn't a valid year\n");
+        if (year <= 0) {
+            System.out.println("'0' isn't a valid year");
+            date = false;
         } else {
             if (month == 2 && (day >= 1 && day <= 28)) {
                 date = true;
-            } else if (month == 4 || month == 6){
-                
+            } else if ((month == 4 || month == 6 || month == 9 || month == 11)
+                    && (day >= 1 && day <= 30)) {
+                date = true;
+            } else if ((month == 1 || month == 3 || month == 5 || month == 7
+                    || month == 8 || month == 10 || month == 12)
+                    && (day >= 1 && day <= 31)) {
+                date = true;
+            } else {
+                date = false;
             }
+
+        }
+        if (date) {
+            System.out.printf("%d/%d/%d is a valid date\n", day, month, year);
+        } else {
+            System.out.println("Invalid date");
         }
     }
 }
