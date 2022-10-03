87bf3083ddcbbd75090b3d3634517e6c739e80bc
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-10-03 14:42:25 +0200

    Add files via upload

--- primer_trimestre/unit2/src/resact_10/GradeResult.java
+++ primer_trimestre/unit2/src/resact_10/GradeResult.java
@@ -1,5 +1,6 @@
 package resact_10;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 /**
@@ -11,8 +12,10 @@ public class GradeResult {
 
     public static void main(String[] args) {
         int grade;
+        double realGrade;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your score: ");
+        sc.useLocale(Locale.US);
+        System.out.print("Enter your score (integer number): ");
         grade = sc.nextInt();
         switch (grade) {
             case 0, 1, 2, 3, 4 -> {
@@ -33,6 +36,25 @@ public class GradeResult {
             default -> {
                 System.out.println("That's not a valid grade");
             }
+        }
+        System.out.println("""
+                           -----------------------------------------------------
+                           Alternative solution (with if-else): """);
+        System.out.print("Enter your score "
+                + "(real number with period, not comma): ");
+        realGrade = sc.nextDouble();
+        if (realGrade >= 5){
+            if(realGrade >= 5 && realGrade < 6){
+                System.out.println("Pass");
+            } else if (realGrade >= 6 && realGrade < 7){
+                System.out.println("Good");
+            } else if (realGrade >= 7 && realGrade < 9){
+                System.out.println("Very good");
+            } else {
+                System.out.println("Outstanding");
+            }
+        }else{
+            System.out.println("Fail");
         }
     }
 }
