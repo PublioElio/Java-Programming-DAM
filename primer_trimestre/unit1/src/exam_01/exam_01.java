bfa8a59d0a351d2aea4eb2ce3f9dc11b10de2e0c
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-10-03 14:02:28 +0200

    Add files via upload

---
+++ primer_trimestre/unit1/src/exam_01/exam_01.java
@@ -0,0 +1,33 @@
+package exam_01;
+
+import java.util.Scanner;
+
+/**
+ * This program asks for two numbers and checks if the bigger one is
+ * divisible by the smallest
+ *
+ * @author Adriano Díaz Benítez <Adriano.Díaz>
+ */
+public class exam_01 {
+
+    public static void main(String[] args) {
+        int num1, num2, biggest, smallest;
+        boolean answer;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("First number: ");
+        num1 = sc.nextInt();
+        System.out.print("Second number: ");
+        num2 = sc.nextInt();
+
+        biggest = num1 > num2 ? num1 : num2;
+        smallest = num1 < num2 ? num1 : num2;
+        /* alternative way, with Math library:
+        Math.max(num1, num2);
+        Math.min(num1, num2);
+         */
+        answer = (biggest % smallest) == 0;
+        System.out.printf("Is '%d' divisible by '%d'?: %b\n",
+                biggest, smallest, answer);
+    }
+
+}
