0c645a3ff01abf6c9fceca909222cc5b07923a56
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-10-05 18:24:45 +0200

    Add files via upload

--- primer_trimestre/unit2/src/resact_13/CheckHour.java
+++ primer_trimestre/unit2/src/resact_13/CheckHour.java
@@ -23,22 +23,27 @@ public class CheckHour {
         System.out.print("Enter seconds: ");
         sec = sc.nextInt();
         sec++;
-        if (sec == 60) {
-            sec = 0;
-            min++;
-            if (min == 60) {
-                min = 0;
-                hour++;
-                if (hour == 24) {
-                    hour = 0;
+        if ((hour > 23 || sec > 60 || min > 60) 
+                || (hour < 0 || sec < 0 || min < 0)) {
+            System.out.print("Error: invalid imput\n");
+        } else {
+            if (sec == 60) {
+                sec = 0;
+                min++;
+                if (min == 60) {
+                    min = 0;
+                    hour++;
+                    if (hour == 24) {
+                        hour = 0;
+                    }
                 }
             }
+            sec2 = (sec < 10) ? "0" + sec : "" + sec;
+            min2 = (min < 10) ? "0" + min : "" + min;
+            hour2 = (hour < 10) ? "0" + hour : "" + hour;
+            System.out.println("Your time, one second later: "
+                    + hour2 + "/" + min2 + "/" + sec2);
         }
-        sec2 = (sec < 10) ? "0" + sec : "" + sec;
-        min2 = (min < 10) ? "0" + min : "" + min;
-        hour2 = (hour < 10) ? "0" + hour : "" + hour;
-        System.out.println("Your time, one second later: "
-                + hour2 + "/" + min2 + "/" + sec2);
     }
 
 }
