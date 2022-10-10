949b8e9bb684d2b61a7a63f2f1ce0220953ad18c
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-10-10 12:58:48 +0200

    Add files via upload

--- primer_trimestre/unit2/src/resact_13/CheckHourRedux.java
+++ primer_trimestre/unit2/src/resact_13/CheckHourRedux.java
@@ -3,29 +3,38 @@ package resact_13;
 import java.util.Scanner;
 
 /**
+ * This program calculates the time after a given quantity of hours, minutes and
+ * seconds
  *
  * @author Adriano Díaz <Adriano.Díaz>
  */
 public class CheckHourRedux {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         int hours, minutes, seconds;
+        String sec2, min2, hour2;
         Scanner sc = new Scanner(System.in);
-//        System.out.print("Enter hours: ");
-//        hours = sc.nextInt();
-//        System.out.println("Enter minutes: ");
-//        minutes = sc.nextInt();
+        System.out.print("Enter hours: ");
+        hours = sc.nextInt();
+        System.out.print("Enter minutes: ");
+        minutes = sc.nextInt();
         System.out.print("Enter seconds: ");
         seconds = sc.nextInt();
-        
-        minutes = seconds / 60;
-        seconds %= 60;
-        
-        hours = minutes / 60;
-        minutes %= 60; 
-        
-        System.out.println(hours);
-        System.out.println(minutes);
-        System.out.println(seconds);
-        
+        if (seconds > 59) {
+            minutes += seconds / 60;
+            seconds %= 60;
+        }
+        if (minutes > 59) {
+            hours += minutes / 60;
+            minutes %= 60;
+
+            hours %= 24;
+        }
+        sec2 = (seconds < 10) ? "0" + seconds : "" + seconds;
+        min2 = (minutes < 10) ? "0" + minutes : "" + minutes;
+        hour2 = (hours < 10) ? "0" + hours : "" + hours;
+        System.out.println("Your time, one second later: "
+                + hour2 + "/" + min2 + "/" + sec2);
+
     }
 }
