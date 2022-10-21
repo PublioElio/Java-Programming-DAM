2d20d561021d0a3138221a5806e032350948ef1a
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-10-21 10:11:35 +0200

    Add files via upload

---
+++ primer_trimestre/unit3/src/appact_18/LeastCommonMultiple.java
@@ -0,0 +1,57 @@
+package appact_18;
+
+import java.util.Scanner;
+
+/**
+ * Calculates the greatest common divisor (GCD) of two integers, then,
+ * calculates the least common multiple
+ *
+ * @author Adriano Díaz Benítez <Adriano.Díaz>
+ */
+public class LeastCommonMultiple {
+
+    public static void main(String[] args) {
+        int a, b, lower, greater, divisor, multiple;
+        a = get_number();
+        b = get_number();
+        lower = a < b ? a : b;
+        greater = a > b ? a : b;
+        divisor = get_GCD(lower, greater);
+        multiple = (a * b) / divisor;
+        System.out.printf("The least common multiple (LCM) of the entered"
+                + " numbers is: %d\n", multiple);
+
+    }
+
+    /**
+     * Ask the user for a positive integer number
+     *
+     * @return the entered int
+     */
+    public static int get_number() {
+        int num;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.print("Enter a positive integer number: ");
+            num = sc.nextInt();
+        } while (num < 0);
+        return (num);
+    }
+
+    /**
+     * Calcutes the GCD from the lower number
+     *
+     * @param lower the lower int enterded by the user
+     * @param greater the greater int enterded by the user
+     * @return the GCD
+     */
+    public static int get_GCD(int lower, int greater) {
+        int i = lower;
+        for (; i > 0; i--) {
+            if (((lower % i) == 0) && ((greater % i) == 0)) {
+                return (i);
+            }
+        }
+        return (i);
+    }
+}
