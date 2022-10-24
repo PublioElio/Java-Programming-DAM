be52b185d79b589b975dfa30719d11ac3369b5f1
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-10-24 23:28:41 +0200

    Add files via upload

--- primer_trimestre/unit3/src/appact_19/SquareRoot.java
+++ primer_trimestre/unit3/src/appact_19/SquareRoot.java
@@ -3,6 +3,8 @@ package appact_19;
 import java.util.Scanner;
 
 /**
+ * Calculates a square root by approximation, in case it is not exact, it gives
+ * the nearest number, plus the rest
  *
  * @author Adriano Díaz Benítez <Adriano.Díaz>
  */
@@ -26,10 +28,19 @@ public class SquareRoot {
             Scanner sc = new Scanner(System.in);
             System.out.print("Enter a positive integer number: ");
             num = sc.nextInt();
+            if (num < 0) {
+                System.out.println("Invalid imput.");
+            }
         } while (num < 0);
         return (num);
     }
 
+    /**
+     * Calculates the square root of a number using the Math Java library. This
+     * is a function to test the exercise
+     *
+     * @param num the number to calculate the sqrt
+     */
     public static void test_sqrt(int num) {
         int sqrt, pow, remainder;
         sqrt = (int) Math.sqrt(num);
@@ -45,11 +56,16 @@ public class SquareRoot {
 
     }
 
+    /**
+     * This is the calculation of the square root via approach
+     *
+     * @param num the calculation of the sqrt
+     */
     public static void get_sqrt(int num) {
         int sqrt, pow, remainder;
         for (sqrt = 1; (sqrt * sqrt) <= num; sqrt++) {
         }
-        sqrt--;
+        sqrt--; // decreasing the index is needed
         pow = sqrt * sqrt;
         remainder = num - pow;
         if (remainder > 0) {
