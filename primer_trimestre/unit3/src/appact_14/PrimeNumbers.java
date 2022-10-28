f2ea956134068fedb6a717e8aac3ec8b62f0c442
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-10-28 14:21:17 +0200

    Add files via upload

--- primer_trimestre/unit3/src/appact_14/PrimeNumbers.java
+++ primer_trimestre/unit3/src/appact_14/PrimeNumbers.java
@@ -20,7 +20,7 @@ public class PrimeNumbers {
         System.out.print("List of prime numbers: 1");
         for (int i = 2; i <= n; i++) {
             boolean isPrime = true;
-            for (int j = 2; j < i; j++) {
+            for (int j = 2; (j < i/2) && isPrime; j++) {
                 if ((i % j) == 0) {
                     isPrime = false;
                 }
