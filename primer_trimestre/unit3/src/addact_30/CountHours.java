fca6eb35613127eac1a3b6d1619af1c52aff0d83
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-11-02 14:40:18 +0100

    Add files via upload

---
+++ primer_trimestre/unit3/src/addact_30/CountHours.java
@@ -0,0 +1,19 @@
+package addact_30;
+
+import java.util.Scanner;
+
+/**
+ * This program calculates the hours between two days of the week. Minutes and
+ * seconds will not be taken into account. The day of the week can be entered as
+ * a number (from 1 to 7) or as a String (from "Monday" to "Sunday"). Check that
+ * the user enters the data correctly and that the second day is later than the
+ * first.
+ *
+ * @author Adriano Díaz Benítez <Adriano.Díaz>
+ */
+public class CountHours {
+
+    public static void main(String[] args) {
+    }
+
+}
