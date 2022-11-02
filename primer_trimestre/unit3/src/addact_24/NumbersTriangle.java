f91bf4bbf73ee989ebf11938ede71bab7e2b63c2
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-11-02 13:14:01 +0100

    Add files via upload

--- primer_trimestre/unit3/src/addact_24/NumbersTriangle.java
+++ primer_trimestre/unit3/src/addact_24/NumbersTriangle.java
@@ -3,27 +3,39 @@ package addact_24;
 import java.util.Scanner;
 
 /**
- *
+ * This program reads a number 'n' and prints a pyramid of numbers with 'n' rows
+ * as in the following figure:
+ *      1
+ *     121 
+ *    12321 
+ *   1234321
  * @author Adriano Díaz Benítez <Adriano.Díaz>
  */
 public class NumbersTriangle {
 
     public static void main(String[] args) {
-        int n, sides;
+        int n;
         n = get_num();
-        for (int i = 0; i < n; i++) {
-            sides = 1;
-            for (int j = i; j < n; j++) {
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = i; j <= n; j++) {
                 System.out.print(" ");
             }
-            for (int k = 0; k <= i; k++) {
-                sides += k;
-                System.out.printf("%d ",sides);
+            for (int k = 1; k < i; k++) {
+                System.out.printf("%d", k);
+            }
+            for (int m = i; m > 0; m--) {
+                System.out.printf("%d", m);
             }
             System.out.println();
         }
     }
 
+    /**
+     * Gets a positive integer number
+     * 
+     * @return a positive integer number
+     */
     static int get_num() {
         int num;
         Scanner sc = new Scanner(System.in);
