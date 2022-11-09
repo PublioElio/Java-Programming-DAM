f5a0375ccd9d2c200a1cd92e5c77b8f13c306ed3
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-11-09 21:52:16 +0100

    Add files via upload

--- primer_trimestre/unit3/src/appact_14/PrimeNumbers.java
+++ primer_trimestre/unit3/src/appact_14/PrimeNumbers.java
@@ -11,26 +11,47 @@ import java.util.Scanner;
 public class PrimeNumbers {
 
     public static void main(String[] args) {
-        int n, counter = 1;
+        int n, counter = 0;
+        boolean isPrime;
+        n = get_num();
+
+        if (n <= 1) {
+            System.out.printf("%d is not a prime number.\n", n);
+        } else {
+            System.out.print("List of prime numbers: ");
+            for (int i = 2; i <= n; i++) {
+                isPrime = true;
+                for (int j = 2; (j <= (i / 2)) && isPrime; j++) {
+                    if ((i % j) == 0) {
+                        isPrime = false;
+                    }
+                }
+                if (isPrime) {
+                    counter++;
+                    System.out.printf("%d ", i);
+                }
+            }
+            System.out.printf("\nBetween 1 and %d there are %d prime "
+                    + "numbers\n", n, counter);
+
+        }
+    }
+
+    /**
+     * This function gets a positive integer number
+     *
+     * @return a positive integer
+     */
+    static int get_num() {
+        int num;
         Scanner sc = new Scanner(System.in);
         do {
             System.out.print("Enter a positive integer number: ");
-            n = sc.nextInt();
-        } while (n < 1); // this do while checks that the imput is valid
-        System.out.print("List of prime numbers: 1");
-        for (int i = 2; i <= n; i++) {
-            boolean isPrime = true;
-            for (int j = 2; (j < i / 2) && isPrime; j++) {
-                if ((i % j) == 0) {
-                    isPrime = false;
-                }
+            num = sc.nextInt();
+            if (num < 0) {
+                System.out.println("The number must be positive. ");
             }
-            if (isPrime) {
-                counter++;
-                System.out.printf(", %d", i);
-            }
-        }
-        System.out.printf("\nBetween 1 and %d there are %d prime numbers\n",
-                n, counter);
+        } while (num < 0);
+        return (num);
     }
 }
