198f475f26ba4b5cbc95247af6185a79ed506a53
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-11-16 14:36:53 +0100

    Add files via upload

---
+++ primer_trimestre/unit4/src/resact_12/RecursiveFibonacci.java
@@ -0,0 +1,18 @@
+package resact_12;
+
+import java.util.Scanner;
+
+/**
+ * This program shows the fibonacci sequence up to position 'n'
+ *
+ * @author Adriano Díaz Benítez <Adriano.Díaz>
+ */
+public class RecursiveFibonacci {
+    public static void main(String[] args){
+        int n;
+        do{
+        n = getNum();
+        }while(n >= 40);
+    }
+
+}
