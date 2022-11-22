e8973ef1f92e56615cb8ab5d874989b2b35fcd16
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-11-22 20:35:52 +0100

    Add files via upload

---
+++ primer_trimestre/unit4/src/addact_18/DecimalToBinaryRedux.java
@@ -0,0 +1,31 @@
+package addact_18;
+
+import static addact_14.CustomMathLib.*;
+
+/**
+ * This program turns a decimal number into binary base
+ *
+ * @author Adriano Díaz <Adriano.Díaz>
+ */
+public class DecimalToBinaryRedux {
+
+    public static void main(String[] args) {
+        int num, binaryNum = 0;
+        System.out.print("Enter a number in decimal base: ");
+        num = getPositiveInt();
+
+        if (num != 0) {
+            binaryNum = 1;
+            while (num > 1) {
+                binaryNum = appendRight(binaryNum, num % 2);
+                num /= 2;
+            }
+        }
+
+        binaryNum = appendRight(binaryNum, 1);
+        binaryNum = reverse(binaryNum);
+        binaryNum = eraseRight(binaryNum, 1);
+
+        System.out.printf("That number in binary is: %d\n", binaryNum);
+    }
+}
