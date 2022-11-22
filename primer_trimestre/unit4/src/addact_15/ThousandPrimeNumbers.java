1907378481a6020165bc837c9c2a8b89d8e7b334
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-11-22 19:19:26 +0100

    Add files via upload

---
+++ primer_trimestre/unit4/src/addact_15/ThousandPrimeNumbers.java
@@ -0,0 +1,29 @@
+package addact_15;
+
+import java.util.Scanner;
+import static addact_14.CustomMathLib.isPrime;
+
+/**
+ * This program shows the first 1000 prime numbers
+ *
+ * @author Adriano Díaz <Adriano.Díaz>
+ */
+public class ThousandPrimeNumbers {
+
+    public static void main(String[] args) {
+        int counter = 0;
+        for (int i = 0; counter < 1000; i++) {
+            if (isPrime(i)) {
+                counter++;
+                if ((counter % 25) == 0) {
+                    System.out.printf("%d.\n", i);
+                } else {
+                    System.out.printf("%d, ", i);
+                }
+            }
+        }
+        System.out.printf("\n---------- Total numbers printed: %d ----------\n",
+                counter);
+    }
+
+}
