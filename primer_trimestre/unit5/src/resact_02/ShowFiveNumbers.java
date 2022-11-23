96a8b3a87d4101e62b84d5be72eb7e170f0a17de
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-11-23 12:20:26 +0100

    Add files via upload

--- primer_trimestre/unit5/src/resact_02/ShowFiveNumbers.java
+++ primer_trimestre/unit5/src/resact_02/ShowFiveNumbers.java
@@ -1,23 +1,50 @@
 package resact_02;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 /**
+ * This program ask the user to fill a array of five decimal numbers and then,
+ * prints the array on console
  *
  * @author Adriano Díaz Benítez <Adriano.Díaz>
  */
 public class ShowFiveNumbers {
 
+    /**
+     * This function ask the user to fill a list of decimal numbers
+     *
+     * @param listLenght the lenght of the list to fill
+     * @return the array
+     */
     static double[] getNumbers(int listLenght) {
         double numList[] = new double[listLenght];
-        
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+
+        for (int i = 0; i < numList.length; i++) {
+            System.out.printf("Enter the decimal number for "
+                    + "position %d: ", i);
+            numList[i] = sc.nextDouble();
+        }
         return (numList);
+    }
+
+    /**
+     * This function prinst a list of numbers
+     *
+     * @param numList the list of numbers that will be printed on console
+     */
+    static void printList(double[] numList) {
+        for (double num : numList) {
+            System.out.print(num + " ");
+        }
     }
 
     public static void main(String[] args) {
         double numList[] = new double[5];
-
         numList = getNumbers(numList.length);
+        printList(numList);
     }
 
 }
