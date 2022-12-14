ea2563420fc42d9bf6d91a0376125103df932adf
Adriano Díaz Benítez <64199230+PublioElio@users.noreply.github.com>
2022-12-14 19:15:37 +0100

    Add files via upload

--- primer_trimestre/unit5/src/addact_10/EvenAndOdds.java
+++ primer_trimestre/unit5/src/addact_10/EvenAndOdds.java
@@ -1,39 +1,72 @@
 package addact_10;
 
-import java.util.Arrays;
-
 /**
+ * This program generates a random array of integers, then sorts it, putting the
+ * even numbers first
  *
  * @author Adriano Díaz Benítez <Adriano.Díaz>
  */
 public class EvenAndOdds {
 
+    /**
+     * This function fills the array with random numbers
+     *
+     * @param array
+     */
     static void fillRandomNumbers(int[] array) {
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) (Math.random() * 101);
         }
     }
 
+    /**
+     * This funtion prints the array on terminal
+     *
+     * @param array
+     */
     static void printArray(int[] array) {
         for (int num : array) {
             System.out.print(num + " ");
         }
     }
 
+    /**
+     * This function arranges the array
+     *
+     * @param array
+     */
     static void arrangeEvenAndOdds(int[] array) {
+        int aux, end = 0;
         for (int i = 0; i < array.length; i++) {
             if (array[i] % 2 == 0) {
-
+                for (int j = i; j > 0 + end; j--) {
+                    swap(array, (j - 1), j);
                 }
+                end++;
             }
         }
+    }
+
+    /**
+     * This function swaps the value of two positions in an array
+     *
+     * @param array
+     * @param i
+     * @param j
+     */
+    static void swap(int[] array, int i, int j) {
+        int aux = array[i];
+        array[i] = array[j];
+        array[j] = aux;
     }
 
     public static void main(String[] args) {
         int array[] = new int[20];
         fillRandomNumbers(array);
+        System.out.println("The unsorted array: ");
         printArray(array);
-        System.out.println("");
+        System.out.println();
+        System.out.println("The array with even numbers first: ");
         arrangeEvenAndOdds(array);
         printArray(array);
     }
